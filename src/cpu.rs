a4c60cc6ed39ea4d924d486726e0fd414892162c
ali <ali@mjuma.com>
2026-09-22 14:38:55 +0100

    fixed your HEAD
    Co-authored-by: Isaac Gravenor <isaac@igtech.uk>

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,6 +1,5 @@
 // Code for the 6502 CPU
 pub struct RAM {
-<<<<<<< HEAD
     pub data: [u8; 0x2000],
 }
 
@@ -11,8 +10,6 @@ pub struct CRegisters {
     pub pc: u16,
     pub sp: u8,
     pub sr: u8,
-=======
-    data: [u8; 0x2000],
 }
 
 pub struct CRegisters {
@@ -22,5 +19,4 @@ pub struct CRegisters {
     pc: u16,
     sp: u8,
     sr: u8,
->>>>>>> a0a4bcedd3f3c3d92caf4de07dfef38ab7cecdc9
 }
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ pub struct MemoryMap {
 impl MemoryMap {
     pub fn new() -> Self {
         Self {
-<<<<<<< HEAD
             ram: cpu::RAM { data: [0; 0x2000] },
             ppu_registers: ppu::PRegisters {
                 ppuctrl: 0,
@@ -31,11 +30,6 @@ impl MemoryMap {
                 sp: 0,
                 sr: 0,
             },
-=======
-            ram: { data: [0; 0x2000] },
-            ppu_registers:  {},
-            cpu_registers: cpu::CRegisters {},
->>>>>>> a0a4bcedd3f3c3d92caf4de07dfef38ab7cecdc9
         }
     }
 
