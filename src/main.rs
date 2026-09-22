a0a4bcedd3f3c3d92caf4de07dfef38ab7cecdc9
ali <ali@mjuma.com>
2026-09-22 13:55:56 +0100

    Moved memory map to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 mod cpu;
 mod ppu;
+
+pub struct MemoryMap {
+    pub ram: cpu::RAM,
+    pub ppu_registers: ppu::PRegisters,
+    pub cpu_registers: cpu::CRegisters,
+}
+
+impl MemoryMap {
+    pub fn new() -> Self {
+        Self {
+            ram: { data: [0; 0x2000] },
+            ppu_registers:  {},
+            cpu_registers: cpu::CRegisters {},
+        }
+    }
+
+    pub fn read(&self, addr: u16) -> u8 {
+        match addr {
+            // NES memory map
+            0x0000..=0x1FFF => self.ram.data[addr],
+            0x2000..=0x3FFF => self.ram.data[addr],
+            0x4000..=0x5FFF => self.ram.data[addr],
+            0x6000..=0x7FFF => self.ram.data[addr],
+            0x8000..=0xFFFF => self.ram.data[addr],
+            _ => 0,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
